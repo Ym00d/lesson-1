59e59341d3ff624741777c068882ccb538f25b33
Ym00d <mattvii.bichck@gmail.com>
2025-11-08 11:32:40 +0200

    3

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-print("Hello World")
-name = input("Введіть своє ім'я: ")
+class Student:
+    def __init__(self, name, age, school, knowledge=0):
+        self.name = name
+        self.age = age
+        self.school = school
+        self.knowledge = knowledge
 
-age = input("Введіть свій вік: ")
+    def study(self):
+        self.knowledge += 10
+        print(f"{self.name} навчається. Рівень знань: {self.knowledge}")
 
-print("Привіт!", name + "!", "Тобі", age, "років.")
+    def work(self):
+        print(f"{self.name} працює, застосовуючи свої знання.")
+
+    def rest(self):
+        print(f"{self.name} відпочиває після навчання.")
+
+student1 = Student("Роман", 17, "Ліцей №16")
+student1.study()
+student1.work()
+student1.rest()
